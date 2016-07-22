773e446113b5e22a8b2a1f83a60cbc0438cfd91f
Yuji Nakayama <nkymyj@gmail.com>
2016-07-22 13:38:02 +0900

    Unskip controller spec

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe BooksController, type: :controller do
   # Book. As you add validations to Book, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { title: 'Foo' }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { title: '' }
   }
 
   # This should return the minimal set of values that should be in the session
